7b60f398b12788f3edfd017b70eef7144c5c2287
Connor McSheffrey <connor.mcsheffrey@gmail.com>
2012-06-23 13:51:07 -0700

    sliders working a lil bit

--- js/script-ck.js
+++ js/script-ck.js
@@ -1,2 +1,111 @@
 /* Author: @mcsheffrey
-*//* Swipe Variables */function beforeSlide(a,b,c){console.log($(b));$("#dcshoes ul li img").css({height:"auto",width:"100%"});sliderExpr=c.slideExpr;slideFlag=!0}function afterSlide(a,b,c){currenSlide=$(b);slideNumber=c.currSlide;currentLeft=$(b).position().left;slideFlag=!1;$("#prev").text(slideNumber+1);$("#next").text(c.slideCount);console.log(c)}function nick(){animating=!1;clearTimeout(timer)}$.fn.cycle.transitions.scrollHorzTouch=function(a,b,c){$("#dcshoes ul li img").css({display:"inline-block",height:"auto",position:"relative",width:"100%"});a.css("overflow","hidden").width();c.before.push(function(a,b,c,d){c.rev&&(d=!d);positionNext=$(b).position();positionCurr=$(a).position();$.fn.cycle.commonReset(a,b,c);positionNext.left>0&&d||positionNext.left<0&&!d?c.cssBefore.left=positionNext.left:c.cssBefore.left=d?b.cycleW-1:1-b.cycleW;positionCurr.left>0&&d||positionCurr.left<0&&!d?c.animOut.left=positionCurr.left:c.animOut.left=d?-a.cycleW:a.cycleW});c.cssFirst.left=0;c.cssBefore.top=0;c.animIn.left=0;c.animOut.top=0};var currenSlide=null,slideNumber=0,currentLeft=0,leftStart=0,sliderExpr,slideFlag=!1;$("#dcshoes ul").cycle({easing:"easeInOutQuint",fx:"scrollHorzTouch",timeout:0,speedIn:400,speedOut:400,slideExpr:"img",next:"#next",prev:"#prev",before:beforeSlide,after:afterSlide,startingSlide:0});$("#dcshoes ul").swipe({swipeMoving:function(a){if(slideFlag)return;var b=currentLeft-a;currenSlide.css("left",b+"px");var c=$(sliderExpr,$("#dcshoes ul")),d;nextSlideLeft=b>0?b-currenSlide.width():b+currenSlide.width();flag=b>0?-1:1;d=slideNumber+flag;if(d<0||d>c.length-1)d=d<0?c.length-1:0;c.eq(d).css("left",nextSlideLeft+"px");c.eq(d).show()},swipeLeft:function(){$("#dcshoes ul").cycle("next")},swipeRight:function(){$("#dcshoes ul").cycle("prev")}});var $projects=$(".section").length,current=0,dir="next",pos=0,animating=!1,timer;$("body.work").bind("mousewheel",function(a,b,c,d){a.preventDefault();if(animating){clearTimeout(timer);timer=setTimeout(nick,100);return!1}if(d<.025){if(pos!==$projects){animating=!0;console.log("start scrolling");timer=setTimeout(nick,100);$.scrollTo(".section:eq("+ ++pos+")",800,{easing:"easeInOutQuint",axis:"y",onAfter:function(){console.log("done scrolling")}})}}else if(d>-0.025&&pos!==0){animating=!0;timer=setTimeout(nick,100);$.scrollTo(".section:eq("+ --pos+")",800,{easing:"easeInOutQuint",axis:"y",onAfter:function(){}})}console.log(dir);console.log($projects);console.log(pos);return!1});$("#work").on("click",function(a){a.preventDefault();$.scrollTo(".section:eq(1)",800,{easing:"easeInOutQuint",axis:"y"});pos++});getTwitters("twitter",{id:"mcsheffrey",count:1,enableLinks:!0,ignoreReplies:!0,clearContents:!0,template:'"%text%" <a href="http://twitter.com/%user_screen_name%/statuses/%id_str%/"> ~ %time%</a>'});
+*//* Swipe Variables */// $.fn.cycle.transitions.scrollHorzTouch = function($cont, $slides, opts) {
+//   $('#bioonc ul li img').css({
+//     'display': 'block',
+//     'height':'auto',
+//     'position': 'absolute',
+//     'max-width':'100%',
+//     'width':'auto'
+//   });
+//   $('#bioonc ul li').css({
+//     'display': 'block'
+//   });
+//   $cont.css('overflow','hidden').width();
+//   opts.before.push(function(curr, next, opts, fwd) {
+//     console.log(opts);
+//     if (opts.rev)
+//       fwd = !fwd;    
+//     positionNext = $(next).position();
+//     positionCurr = $(curr).position();
+//     var backgroundWidth = $('#bioonc #bg-scroll').width(),
+//       sectionWidth = $('#bioonc').width(),
+//       bgScrollWidth = ((backgroundWidth - sectionWidth) / opts.slideCount) * (opts.currSlide);
+//   console.log(backgroundWidth, sectionWidth, bgScrollWidth);
+//   $('#bioonc #bg-scroll').css('left', '-' + bgScrollWidth + 'px');
+//   $('#bioonc ul').css('left', '-' + sectionWidth + 'px');
+//     $.fn.cycle.commonReset(curr,next,opts);
+//     if( ( positionNext.left > 0 && fwd ) || ( positionNext.left <  0 && !fwd ) ) {
+//       opts.cssBefore.left = positionNext.left;
+//     } else {
+//       opts.cssBefore.left = fwd ? ( next.cycleW - 1) : ( 1 - next.cycleW );
+//     }
+//     if( ( positionCurr.left > 0 && fwd ) || ( positionCurr.left <  0 && !fwd ) ) {
+//       opts.animOut.left = positionCurr.left;
+//     } else {
+//       opts.animOut.left = fwd ? -curr.cycleW : curr.cycleW;
+//     }
+//     });
+//     opts.cssFirst.left = 0;
+//     opts.cssBefore.top = 0;
+//     opts.animIn.left = 0;
+//     opts.animOut.top = 0;
+//   };
+//   var currenSlide = null;
+//   var slideNumber = 0;
+//   var currentLeft = 0;
+//   var leftStart = 0;
+//   var sliderExpr;
+//   var slideFlag = false;
+//  $('#bioonc ul').cycle({
+//     easing: 'easeInOutQuint',
+//     fx:     'scrollHorzTouch',
+//     timeout: 0,
+//     speedIn:   400,
+//     speedOut:   400,
+//     next: '#next',
+//     prev: '#prev',
+//     before: beforeSlide,
+//     after: afterSlide,
+//     startingSlide: 0
+// });
+// $('#bioonc ul').swipe({ 
+//   swipeMoving: function( pageX ){
+//     if( slideFlag ) return;
+//     var newLeft = currentLeft - pageX;
+//     currenSlide.css('left', newLeft+'px'  );
+//     var $slides = $( sliderExpr, $('#dcshoes ul') );
+//     var scrollSlide;
+//     nextSlideLeft =   newLeft > 0 ? newLeft - currenSlide.width(): newLeft+currenSlide.width();
+//     flag = newLeft > 0 ? -1: 1;
+//     scrollSlide  = slideNumber + flag;
+//     if( scrollSlide < 0 || scrollSlide > ($slides.length - 1 ) )
+//     {
+//         scrollSlide = scrollSlide < 0 ? $slides.length - 1 : 0;
+//     }
+//      $slides.eq( scrollSlide ).css('left',nextSlideLeft + 'px' );
+//      $slides.eq( scrollSlide ).show();
+//   },
+//   swipeLeft: function() {
+//     $('#bioonc ul').cycle('next');
+//   },
+//   swipeRight: function() { 
+//     $('#bioonc ul').cycle('prev'); 
+//   }
+// });
+// $('#hero-prev').on('click', function(event) {
+//   $('#bioonc ul').cycle('prev');
+//   event.preventDefault();
+// });
+// $('#hero-next').on('click', function(event) {
+//   $('#bioonc ul').cycle('next');
+//   event.preventDefault();
+// });
+// // Call this function before the slide start
+// function beforeSlide( curr, next , opt ) {
+//   $('#bioonc ul li img').css({
+//     'height':'auto'
+//   });
+//   sliderExpr = opt.slideExpr;
+//   slideFlag = true;
+// }
+// // Call this function after the slide start
+// function afterSlide(curr, next , opt ) {
+//   currenSlide =  $(next);
+//   slideNumber = opt.currSlide;
+//   currentLeft = $(next).position().left;
+//   slideFlag = false;
+//   $('#prev').text(slideNumber + 1);
+//   $('#next').text(opt.slideCount);
+// }
+// the procedural method
+function nick(){animating=!1;clearTimeout(timer)}(function(e){function a(t){var a=i;t==="next"?++o:--o;if(o===0){o=s;a=u-i;t="next"}else if(o-1===s){o=1;a=0}f(n,a,t);console.log(o);e(r).eq(o).addClass("active").children().removeClass("active")}function f(t,n,r){var i,u=e("#bioonc #bg-scroll").width(),a=e("#bioonc").width(),f=(u-a)/s*o;console.log(r);r==="next"&&t.css({"-webkit-transform":"translate3d("+o+"px,0,0)"});console.log(i,n);r==="next"?e("#bioonc #bg-scroll").css({"-webkit-transform":"translate3d("+ -f+"px,0,0)"}):e("#bioonc #bg-scroll").css({"-webkit-transform":"translate3d("+f+"px,0,0)"})}var t=e("#bioonc"),n=e("#bioonc").find(".hero ul"),r=n.children("li"),i=e(r).eq(0).outerWidth(!0),s=r.length,o=1,u=s*i;console.log(n,r,i,s,o,u);e(n).width(u);t.find("#next").text(s);e("#bioonc").find("button").on("click",function(){var t=e(this).data("dir");a(t)});n.swipe({swipeMoving:function(e){console.log(e)},swipeLeft:function(){a("prev")},swipeRight:function(){a("next")}})})(jQuery);var $projects=$(".section").length,current=0,dir="next",pos=0,animating=!1,timer;$("body.work").bind("mousewheel",function(e,t,n,r){e.preventDefault();if(animating){clearTimeout(timer);timer=setTimeout(nick,50);return!1}if(r<.025){if(pos!==$projects){animating=!0;console.log("start scrolling");timer=setTimeout(nick,50);$.scrollTo(".section:eq("+ ++pos+")",600,{easing:"easeInOutQuart",axis:"y",onAfter:function(){console.log("done scrolling")}})}}else if(r>-0.025&&pos!==0){animating=!0;timer=setTimeout(nick,50);$.scrollTo(".section:eq("+ --pos+")",600,{easing:"easeInOutQuart",axis:"y",onAfter:function(){}})}console.log(dir);console.log($projects);console.log(pos);return!1});$("#work").on("click",function(e){e.preventDefault();$.scrollTo(".section:eq(1)",600,{easing:"easeInOutQuart",axis:"y"});pos++});getTwitters("twitter",{id:"mcsheffrey",count:1,enableLinks:!0,ignoreReplies:!0,clearContents:!0,template:'"%text%" <a href="http://twitter.com/%user_screen_name%/statuses/%id_str%/"> ~ %time%</a>'});
--- js/script.js
+++ js/script.js
@@ -2,123 +2,238 @@
 */
 
 /* Swipe Variables */
-$.fn.cycle.transitions.scrollHorzTouch = function($cont, $slides, opts) {
+// $.fn.cycle.transitions.scrollHorzTouch = function($cont, $slides, opts) {
 
-  $('#dcshoes ul li img').css({
-    'display': 'inline-block',
-    'height':'auto',
-    'position': 'relative',
-    'width':'100%'
+//   $('#bioonc ul li img').css({
+//     'display': 'block',
+//     'height':'auto',
+//     'position': 'absolute',
+//     'max-width':'100%',
+//     'width':'auto'
+//   });
+
+//   $('#bioonc ul li').css({
+//     'display': 'block'
+//   });
+
+//   $cont.css('overflow','hidden').width();
+//   opts.before.push(function(curr, next, opts, fwd) {
+
+//     console.log(opts);
+    
+
+//     if (opts.rev)
+//       fwd = !fwd;    
+
+//     positionNext = $(next).position();
+//     positionCurr = $(curr).position();
+
+//     var backgroundWidth = $('#bioonc #bg-scroll').width(),
+//       sectionWidth = $('#bioonc').width(),
+//       bgScrollWidth = ((backgroundWidth - sectionWidth) / opts.slideCount) * (opts.currSlide);
+
+//   console.log(backgroundWidth, sectionWidth, bgScrollWidth);
+
+//   $('#bioonc #bg-scroll').css('left', '-' + bgScrollWidth + 'px');
+
+//   $('#bioonc ul').css('left', '-' + sectionWidth + 'px');
+
+//     $.fn.cycle.commonReset(curr,next,opts);
+
+//     if( ( positionNext.left > 0 && fwd ) || ( positionNext.left <  0 && !fwd ) ) {
+//       opts.cssBefore.left = positionNext.left;
+
+//     } else {
+//       opts.cssBefore.left = fwd ? ( next.cycleW - 1) : ( 1 - next.cycleW );
+//     }
+
+//     if( ( positionCurr.left > 0 && fwd ) || ( positionCurr.left <  0 && !fwd ) ) {
+//       opts.animOut.left = positionCurr.left;
+//     } else {
+//       opts.animOut.left = fwd ? -curr.cycleW : curr.cycleW;
+//     }
+
+//     });
+
+//     opts.cssFirst.left = 0;
+//     opts.cssBefore.top = 0;
+//     opts.animIn.left = 0;
+//     opts.animOut.top = 0;
+//   };
+
+//   var currenSlide = null;
+//   var slideNumber = 0;
+//   var currentLeft = 0;
+//   var leftStart = 0;
+//   var sliderExpr;
+//   var slideFlag = false;
+
+//  $('#bioonc ul').cycle({
+//     easing: 'easeInOutQuint',
+//     fx:     'scrollHorzTouch',
+//     timeout: 0,
+//     speedIn:   400,
+//     speedOut:   400,
+//     next: '#next',
+//     prev: '#prev',
+//     before: beforeSlide,
+//     after: afterSlide,
+//     startingSlide: 0
+// });
+
+
+// $('#bioonc ul').swipe({ 
+//   swipeMoving: function( pageX ){
+
+//     if( slideFlag ) return;
+
+//     var newLeft = currentLeft - pageX;
+
+//     currenSlide.css('left', newLeft+'px'  );
+
+//     var $slides = $( sliderExpr, $('#dcshoes ul') );
+//     var scrollSlide;
+
+//     nextSlideLeft =   newLeft > 0 ? newLeft - currenSlide.width(): newLeft+currenSlide.width();
+//     flag = newLeft > 0 ? -1: 1;
+//     scrollSlide  = slideNumber + flag;
+//     if( scrollSlide < 0 || scrollSlide > ($slides.length - 1 ) )
+//     {
+//         scrollSlide = scrollSlide < 0 ? $slides.length - 1 : 0;
+//     }
+
+//      $slides.eq( scrollSlide ).css('left',nextSlideLeft + 'px' );
+//      $slides.eq( scrollSlide ).show();
+//   },
+//   swipeLeft: function() {
+
+//     $('#bioonc ul').cycle('next');
+//   },
+//   swipeRight: function() { 
+//     $('#bioonc ul').cycle('prev'); 
+//   }
+// });
+
+// $('#hero-prev').on('click', function(event) {
+//   $('#bioonc ul').cycle('prev');
+
+//   event.preventDefault();
+// });
+
+// $('#hero-next').on('click', function(event) {
+//   $('#bioonc ul').cycle('next');
+  
+//   event.preventDefault();
+// });
+
+
+// // Call this function before the slide start
+// function beforeSlide( curr, next , opt ) {
+
+//   $('#bioonc ul li img').css({
+//     'height':'auto'
+//   });
+//   sliderExpr = opt.slideExpr;
+//   slideFlag = true;
+// }
+
+// // Call this function after the slide start
+// function afterSlide(curr, next , opt ) {
+//   currenSlide =  $(next);
+//   slideNumber = opt.currSlide;
+//   currentLeft = $(next).position().left;
+//   slideFlag = false;
+//   $('#prev').text(slideNumber + 1);
+//   $('#next').text(opt.slideCount);
+// }
+
+// the procedural method
+(function($) {
+  var $section = $('#bioonc'),
+      sliderUL = $('#bioonc').find('.hero ul'),
+      imgs = sliderUL.children('li'),
+      imgWidth = $(imgs).eq(0).addClass('active').outerWidth(true), // 600
+      imgsLen = imgs.length, // 4
+      current = 1,
+      totalImgsWidth = imgsLen * imgWidth; // 2400
+
+      console.log(sliderUL, imgs, imgWidth, imgsLen, current, totalImgsWidth);
+
+  $(sliderUL).width(totalImgsWidth);
+
+  $section.find('#next').text(imgsLen);
+
+  $('#bioonc').find('button').on('click', function() {
+
+    var direction = $(this).data('dir');
+
+    setCurrent(direction)
   });
 
-  $cont.css('overflow','hidden').width();
-  opts.before.push(function(curr, next, opts, fwd) {
+  function setCurrent(direction) {
+    var loc = imgWidth; // 600
 
-  if (opts.rev)
-    fwd = !fwd;
+    // update current value
+    ( direction === 'next' ) ? ++current : --current;
 
-  positionNext = $(next).position();
-  positionCurr = $(curr).position();
-
-  $.fn.cycle.commonReset(curr,next,opts);
-
-  if( ( positionNext.left > 0 && fwd ) || ( positionNext.left <  0 && !fwd ) ) {
-    opts.cssBefore.left = positionNext.left;
-  } else {
-    opts.cssBefore.left = fwd ? (next.cycleW-1) : (1-next.cycleW);
-  }
-
-  if( ( positionCurr.left > 0 && fwd ) || ( positionCurr.left <  0 && !fwd ) ) {
-    opts.animOut.left = positionCurr.left;
-  } else {
-    opts.animOut.left = fwd ? -curr.cycleW : curr.cycleW;
-  }
-
-});
-
-  opts.cssFirst.left = 0;
-  opts.cssBefore.top = 0;
-  opts.animIn.left = 0;
-  opts.animOut.top = 0;
-};
-  var currenSlide = null;
-  var slideNumber = 0;
-  var currentLeft = 0;
-  var leftStart = 0;
-  var sliderExpr;
-  var slideFlag = false;
-
- $('#dcshoes ul').cycle({
-    easing: 'easeInOutQuint',
-    fx:     'scrollHorzTouch',
-    timeout: 0,
-    speedIn:   400,
-    speedOut:   400,
-    slideExpr: 'img',
-    next: '#next',
-    prev: '#prev',
-    before: beforeSlide,
-    after: afterSlide,
-    startingSlide: 0
-});
-
-
-$('#dcshoes ul').swipe({ 
-  swipeMoving: function( pageX ){
-
-    if( slideFlag ) return;
-
-    var newLeft = currentLeft-pageX;
-
-    currenSlide.css('left', newLeft+'px'  );
-
-    var $slides = $( sliderExpr, $('#dcshoes ul') );
-    var scrollSlide;
-
-    nextSlideLeft =   newLeft > 0 ? newLeft - currenSlide.width(): newLeft+currenSlide.width();
-    flag = newLeft > 0 ? -1: 1;
-    scrollSlide  = slideNumber + flag;
-    if( scrollSlide < 0 || scrollSlide > ($slides.length - 1 ) )
-    {
-        scrollSlide = scrollSlide < 0 ? $slides.length - 1 : 0;
+    // if first image
+    if ( current === 0 ) {
+      current = imgsLen;
+      loc = totalImgsWidth - imgWidth; // 2400 - 600 = 1800
+      direction = 'next';
+    } else if ( current - 1 === imgsLen ) { // Are we at end? Should we reset?
+      current = 1;
+      loc = 0;
     }
 
-     $slides.eq( scrollSlide ).css('left',nextSlideLeft + 'px' );
-     $slides.eq( scrollSlide ).show();
-  },
-  swipeLeft: function() {
-    $('#dcshoes ul').cycle('next');
-  },
-  swipeRight: function() { 
-    $('#dcshoes ul').cycle('prev'); 
+    transition(sliderUL, loc, direction);
+    console.log(current);
+
+    $section.find('#prev').text(current);
+
+    $(imgs).eq(current-1).addClass('active').siblings().removeClass('active');
   }
-});
 
+  function transition( container, loc, direction ) {
+    var unit, // -= +=
+        backgroundWidth = $('#bioonc #bg-scroll').width(),
+        sectionWidth = $('#bioonc').width(),
+        bgScrollWidth = ((backgroundWidth - sectionWidth) / imgsLen) * current;
 
-// Call this function before the slide start
-function beforeSlide( curr, next , opt ) {
-  console.log($(next));
-  
-  $('#dcshoes ul li img').css({
-    'height':'auto',
-    'width':'100%'
+    if (direction === 'next') {
+      container.css({
+        '-webkit-transform': 'translate3d(' + (current-1) * -imgWidth + 'px,0,0)'
+      });
+    }
+
+    console.log(unit, loc);
+
+    if (direction === 'next') {
+      $('#bioonc #bg-scroll').css({
+        '-webkit-transform': 'translate3d(' + -bgScrollWidth + 'px,0,0)'
+      });
+    } else {
+      $('#bioonc #bg-scroll').css({
+        '-webkit-transform': 'translate3d(' + bgScrollWidth + 'px,0,0)'
+      });
+    }
+
+  }
+
+  sliderUL.swipe({
+    swipeMoving: function( pageX ){
+      console.log(pageX);
+      
+    },
+    swipeLeft: function() {
+      setCurrent('prev')
+    },
+    swipeRight: function() {
+      setCurrent('next')
+    }
   });
-  sliderExpr = opt.slideExpr;
-  slideFlag = true;
-}
 
-// Call this function after the slide start
-function afterSlide(curr, next , opt ) {
-  currenSlide =  $(next);
-  slideNumber = opt.currSlide;
-  currentLeft = $(next).position().left;
-  slideFlag = false;
-  $('#prev').text(slideNumber + 1);
-  $('#next').text(opt.slideCount);
-  console.log(opt);
-  
-}
-
+})(jQuery);
 
 var $projects = $('.section').length,
     current = 0,
@@ -127,6 +242,7 @@ var $projects = $('.section').length,
     animating = false,
     timer;
 
+    // Fake timer
     function nick() {
       animating = false;
 
@@ -141,7 +257,7 @@ $('body.work').bind("mousewheel", function(event, delta, deltaX, deltaY) {
   if( animating ) {
     clearTimeout(timer);
 
-    timer = setTimeout(nick, 100);
+    timer = setTimeout(nick, 50);
 
     return false;
   }
@@ -154,13 +270,12 @@ $('body.work').bind("mousewheel", function(event, delta, deltaX, deltaY) {
 
         console.log('start scrolling');
 
-        timer = setTimeout(nick, 100);
+        timer = setTimeout(nick, 50);
 
-        $.scrollTo('.section:eq(' + ++pos + ')', 800, {easing:'easeInOutQuint',axis: "y", onAfter:function(){
+        $.scrollTo('.section:eq(' + ++pos + ')', 600, {easing:'easeInOutQuart',axis: "y", onAfter:function(){
 
           console.log('done scrolling');
           
-
         }});
       } 
 
@@ -170,9 +285,9 @@ $('body.work').bind("mousewheel", function(event, delta, deltaX, deltaY) {
 
         animating = true;
 
-        timer = setTimeout(nick, 100);
+        timer = setTimeout(nick, 50);
 
-        $.scrollTo('.section:eq(' + --pos + ')', 800, {easing:'easeInOutQuint',axis: "y", onAfter:function(){
+        $.scrollTo('.section:eq(' + --pos + ')', 600, {easing:'easeInOutQuart',axis: "y", onAfter:function(){
 
         }});
       }  
@@ -194,7 +309,7 @@ $('body.work').bind("mousewheel", function(event, delta, deltaX, deltaY) {
 
 $('#work').on('click', function(event) {
   event.preventDefault();
-  $.scrollTo('.section:eq(1)', 800, {easing:'easeInOutQuint',axis: "y"});
+  $.scrollTo('.section:eq(1)', 600, {easing:'easeInOutQuart',axis: "y"});
   pos++;
 });
 
